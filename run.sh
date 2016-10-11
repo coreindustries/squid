cdeeacbebcaa20c5ff75dc73f4e0afb7fa91f53d
corey szopinski <corey@core-industries.com>
2016-10-11 13:59:12 -0700

    allowing port 3000 apps

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 docker run --name squid -d --restart=always \
   --publish 3128:3128 \
   --volume /tmp/squid/cache:/var/spool/squid3 \
+  --volume /Users/coreysi/sites/dockers/squid/squid.conf:/etc/squid3/squid.conf \
   sameersbn/squid:3.3.8-19
