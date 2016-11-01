34f58e5d2d287fdc340ea040c0d766152a50a72a
corey szopinski <corey@core-industries.com>
2016-11-01 11:10:12 -0700

    updating to cache onto raid instead of tmp

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
+docker rm squid
+# docker run --name squid -d --restart=always \
+#   --publish 3128:3128 \
+#   --volume /tmp/squid/cache:/var/spool/squid3 \
+#   sameersbn/squid:3.3.8-19
+
 docker run --name squid -d --restart=always \
   --publish 3128:3128 \
-  --volume /tmp/squid/cache:/var/spool/squid3 \
+  --volume /mnt/raid/cache:/var/spool/squid3 \
   sameersbn/squid:3.3.8-19
